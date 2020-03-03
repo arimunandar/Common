b695c7436ca7a5dc1e085ed4ef3c93e30a44a55a
Ari Munandar <arimunandar.dev@gmail.com>
2020-03-03 17:24:00 +0700

    Add Image

--- Sources/Common/Common.swift
+++ Sources/Common/Common.swift
@@ -1,5 +1,15 @@
-public struct Common {
+import UIKit
+
+public class Common {
     public var text = "Hello, World!"
     
     public init() {}
+    
+    func getImage() -> UIImage? {
+        let bundle = Bundle(for: type(of: self))
+        guard let image = UIImage(named: "test", in: bundle, compatibleWith: nil) else {
+            return nil
+        }
+        return image
+    }
 }
